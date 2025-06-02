f4743dc2c21a79b422427621b65b8fffbce3d566
Julian Cravero <juliancravero@hotmail.com>
2025-06-02 12:27:40 -0300

    Correcciones css

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,12 @@ function App() {
       <Router>
         <Navbar />
         <div className="navbar-spacer" />
-        <div className="layout">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
 
         <Footer />
       </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,9 @@ import HeroSlider from "../components/HeroSlider";
 
 export default function Home() {
   return (
-    <div>
-      <HeroSlider />
-      <section className="mt-10 max-w-4xl mx-auto text-center">
-        <h2 className="text-2xl font-semibold mb-4 filamentos-title"></h2>
-      </section>
-      <GallerySection /> 
-    </div>
+    <>
+    <HeroSlider />
+    <GallerySection /> 
+    </>
   );
 }
